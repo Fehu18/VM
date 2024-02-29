fcdfbc57295f94b3249dc35d2470383aeae468b8
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 11:54:01 +0530

    Update main.tf

--- name/main.tf
+++ name/main.tf
@@ -9,20 +9,14 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "storagerg"
-    storage_account_name = "backendstore106"
-    container_name       = "storecont"
-    key                  = "terraform.tfstate"
-  }
-
+ 
 }
 provider "azurerm" {
   features {}
   subscription_id = "dd841e0c-a76f-4d5d-ae2f-b0645b248ae5"
   tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
 }
-}
+
 
 
 locals {
