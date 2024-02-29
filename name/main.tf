b3383cedd1324a0adc4f9fc61bc254063b53f50a
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 20:08:24 +0530

    Create main.tf

---
+++ name/main.tf
@@ -0,0 +1,139 @@
+
+terraform {
+
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "storagergs"
+    storage_account_name = "backendstore106"
+    container_name       = "storecont1"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "dd841e0c-a76f-4d5d-ae2f-b0645b248ae5"
+  tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "service" {
+  name                 = "service"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  enforce_private_link_service_network_policies = true
+}
+
+resource "azurerm_subnet" "endpoint" {
+  name                 = "endpoint"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  enforce_private_link_endpoint_network_policies = true
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-pip"
+  sku                 = "Standard"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+
+resource "azurerm_network_interface" "app_interface" {
+  name                = "app-interface"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.service.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+
+  depends_on = [
+    azurerm_virtual_network.app_network,
+    azurerm_public_ip.example
+  ]
+}
+
+
+resource "azurerm_windows_virtual_machine" "app_vm" {
+  name                = "appvm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_D2s_v3"
+  admin_username      = "demousr"
+  admin_password      = "Admin@123"
+  network_interface_ids = [
+    azurerm_network_interface.app_interface.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  depends_on = [
+    azurerm_network_interface.app_interface,
+   
+  ]
+}
+
+
+resource "azurerm_private_link_service" "example" {
+  name                = "example-privatelink"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  nat_ip_configuration {
+    name      = azurerm_public_ip.example.name
+    primary   = true
+    subnet_id = azurerm_subnet.service.id
+  }
+
+
+resource "azurerm_private_endpoint" "example" {
+  name                = "example-endpoint"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_id           = azurerm_subnet.endpoint.id
+
+  private_service_connection {
+    name                           = "example-privateserviceconnection"
+    private_connection_resource_id = azurerm_private_link_service.example.id
+    is_manual_connection           = false
+  }
+}
