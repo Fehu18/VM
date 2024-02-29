30f90b233a0dcabca19eb30fd485f967d26600bc
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 12:53:01 +0530

    Update main.tf

--- name/main.tf
+++ name/main.tf
@@ -17,16 +17,14 @@ provider "azurerm" {
   tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
 }
 
-
-
 locals {
-  resource_group="app-test"
+  resource_group="app-grp"
   location="North Europe"
 }
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "app_test"{
+resource "azurerm_resource_group" "app_grp"{
   name=local.resource_group
   location=local.location
 }
@@ -34,7 +32,7 @@ resource "azurerm_resource_group" "app_test"{
 resource "azurerm_virtual_network" "app_network" {
   name                = "app-network"
   location            = local.location
-  resource_group_name = azurerm_resource_group.app_test.name
+  resource_group_name = azurerm_resource_group.app_grp.name
   address_space       = ["10.0.0.0/16"]
 }
 
@@ -97,36 +95,38 @@ resource "azurerm_windows_virtual_machine" "app_vm" {
 
 resource "azurerm_public_ip" "app_public_ip" {
   name                = "app-public-ip"
-  resource_group_name = "app-test"
+  resource_group_name = local.resource_group
   location            = local.location
   allocation_method   = "Static"
   depends_on = [
-    azurerm_resource_group.app_test
+    azurerm_resource_group.app_grp
   ]
 }
 
 resource "azurerm_key_vault" "app_vault" {  
-  name                        = "appvault908942"
+  name                        = "appvault9087878"
   location                    = local.location
   resource_group_name         = local.resource_group  
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   sku_name = "standard"
-  
-
 access_policy {
     tenant_id = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
     object_id = "4bbcfb7b-8415-4828-aee5-c19aa18500d4"
 
     secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
-  }
+  
+     }
+
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
 }
 
 # We are creating a secret in the key vault
 resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = "Azure@123"
-  key_vault_id = azurerm_key_vault.app_vault.id
+  key_vault_id = azurerm_key_vault.app_vaut.id
   depends_on = [ azurerm_key_vault.app_vault ]
-}
