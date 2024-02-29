e9c7dcb35fa65d545e8fb852e8ec07430e57bbf3
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 10:31:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,147 @@
+terraform {
+
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "storagerg"
+    storage_account_name = "backendstore106"
+    container_name       = "storecont"
+    key                  = "terraform.tfstate"
+  }
+
+}
+provider "azurerm" {
+  subscription_id = "dd841e0c-a76f-4d5d-ae2f-b0645b248ae5"
+  client_id       = "45170848-56f9-4b9a-b7ac-6453803390fe"
+  client_secret   = "2c9a5c4a-477d-4dd7-b031-94851734b755"
+  tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
+  features {}
+}
+
+
+locals {
+  resource_group="app-grp"
+  location="North Europe"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "app_grp"{
+  name=local.resource_group
+  location=local.location
+}
+
+resource "azurerm_virtual_network" "app_network" {
+  name                = "app-network"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "SubnetA" {
+  name                 = "SubnetA"
+  resource_group_name  = local.resource_group
+  virtual_network_name = azurerm_virtual_network.app_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on = [
+    azurerm_virtual_network.app_network
+  ]
+}
+
+resource "azurerm_network_interface" "app_interface" {
+  name                = "app-interface"
+  location            = local.location
+  resource_group_name = local.resource_group
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.SubnetA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.app_public_ip.id
+  }
+
+  depends_on = [
+    azurerm_virtual_network.app_network,
+    azurerm_public_ip.app_public_ip
+  ]
+}
+
+resource "azurerm_windows_virtual_machine" "app_vm" {
+  name                = "appvm"
+  resource_group_name = local.resource_group
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "demousr"
+  admin_password      = azurerm_key_vault_secret.vmpassword.value
+  network_interface_ids = [
+    azurerm_network_interface.app_interface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  depends_on = [
+    azurerm_network_interface.app_interface,
+    azurerm_key_vault_secret.vmpassword
+  ]
+}
+
+resource "azurerm_public_ip" "app_public_ip" {
+  name                = "app-public-ip"
+  resource_group_name = local.resource_group
+  location            = local.location
+  allocation_method   = "Static"
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
+}
+
+resource "azurerm_key_vault" "app_vault" {  
+  name                        = "appvault9087123"
+  location                    = local.location
+  resource_group_name         = local.resource_group  
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "get",
+    ]
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+    storage_permissions = [
+      "get",
+    ]
+  }
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
+}
+
+# We are creating a secret in the key vault
+resource "azurerm_key_vault_secret" "vmpassword" {
+  name         = "vmpassword"
+  value        = "Azure@123"
+  key_vault_id = azurerm_key_vault.app_vault.id
+  depends_on = [ azurerm_key_vault.app_vault ]
+}
