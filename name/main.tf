52e5781dac732f3b0c0d1fd85b77c56725788b89
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-03-02 05:11:05 +0530

    Update main.tf

--- name/main.tf
+++ name/main.tf
@@ -1,86 +1,77 @@
-
 terraform {
-
-  required_version = ">=1.0"
-
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      source = "hashicorp/azurerm"
+      version = "2.93.0"
     }
   }
 }
+
 provider "azurerm" {
-  features {}
   subscription_id = "dd841e0c-a76f-4d5d-ae2f-b0645b248ae5"
+  client_id       = "45170848-56f9-4b9a-b7ac-6453803390fe"
+  client_secret   = "3b7d174e-56ad-4b3c-a5f2-18f79423ae6f"
   tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
+  features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+
+locals {
+  resource_group="app-grp"
+  location="North Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "app_grp"{
+  name=local.resource_group
+  location=local.location
+}
+
+resource "azurerm_virtual_network" "app_network" {
+  name                = "app-network"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.app_grp.name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "service" {
-  name                 = "service"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "SubnetA" {
+  name                 = "SubnetA"
+  resource_group_name  = local.resource_group
+  virtual_network_name = azurerm_virtual_network.app_network.name
   address_prefixes     = ["10.0.1.0/24"]
-
-}
-
-resource "azurerm_subnet" "endpoint" {
-  name                 = "endpoint"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "example-pip"
-  sku                 = "Standard"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-}
-
-
-resource "azurerm_network_interface" "app_interface" {
-  name                = "app-interface"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.service.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
-
   depends_on = [
-    azurerm_virtual_network.example,
-    azurerm_public_ip.example
+    azurerm_virtual_network.app_network
   ]
 }
 
+resource "azurerm_network_interface" "app_interface" {
+  name                = "app-interface"
+  location            = local.location
+  resource_group_name = local.resource_group
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.SubnetA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.app_public_ip.id
+  }
+
+  depends_on = [
+    azurerm_virtual_network.app_network,
+    azurerm_public_ip.app_public_ip
+  ]
+}
 
 resource "azurerm_windows_virtual_machine" "app_vm" {
   name                = "appvm"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = local.resource_group
+  location            = local.location
   size                = "Standard_D2s_v3"
   admin_username      = "demousr"
-  admin_password      = "Admin@123"
+  admin_password      = azurerm_key_vault_secret.vmpassword.value
   network_interface_ids = [
-    azurerm_network_interface.app_interface.id
+    azurerm_network_interface.app_interface.id,
   ]
 
   os_disk {
@@ -97,46 +88,50 @@ resource "azurerm_windows_virtual_machine" "app_vm" {
 
   depends_on = [
     azurerm_network_interface.app_interface,
-   
-  ]
-}
-resource "azurerm_lb" "example" {
-  name                = "example-lb"
-  sku                 = "Standard"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  frontend_ip_configuration {
-    name                 = azurerm_public_ip.example.name
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
-}
-
-resource "azurerm_private_link_service" "example" {
-  name                = "example-privatelink"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  nat_ip_configuration {
-    name      = azurerm_public_ip.example.name
-    primary   = true
-    subnet_id = azurerm_subnet.service.id
-  }
-load_balancer_frontend_ip_configuration_ids = [
-    azurerm_lb.example.frontend_ip_configuration.0.id,
+    azurerm_key_vault_secret.vmpassword
   ]
 }
 
+resource "azurerm_public_ip" "app_public_ip" {
+  name                = "app-public-ip"
+  resource_group_name = local.resource_group
+  location            = local.location
+  allocation_method   = "Static"
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
+}
 
-resource "azurerm_private_endpoint" "example" {
-  name                = "example-endpoint"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  subnet_id           = azurerm_subnet.endpoint.id
-
-  private_service_connection {
-    name                           = "example-privateserviceconnection"
-    private_connection_resource_id = azurerm_private_link_service.example.id
-    is_manual_connection           = false
+resource "azurerm_key_vault" "app_vault" {  
+  name                        = "appvault9087878"
+  location                    = local.location
+  resource_group_name         = local.resource_group  
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "get",
+    ]
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+    storage_permissions = [
+      "get",
+    ]
   }
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
+}
+
+# We are creating a secret in the key vault
+resource "azurerm_key_vault_secret" "vmpassword" {
+  name         = "vmpassword"
+  value        = "Azure@123"
+  key_vault_id = azurerm_key_vault.app_vault.id
+  depends_on = [ azurerm_key_vault.app_vault ]
 }
