652f5e68521c6d3b0725a839cb51f53ed97cd56c
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-03-02 06:01:35 +0530

    Update main.tf

--- name/main.tf
+++ name/main.tf
@@ -111,7 +111,7 @@ resource "azurerm_key_vault" "app_vault" {
 }
 
 resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id = azurerm_key_vault.example.id
+  key_vault_id = azurerm_key_vault.app_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
